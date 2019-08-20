a9468b5fdeffaf3984f494f6a123dd74e6d4c1c6
cybacraft <ukonudennis63@gmail.com>
2019-08-20 17:05:58 +0100

    reactHookes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Todo({ todo, index, completeTodo, removeTodo}) {
+  return <div style={{textDecoration: todo.isCompleted? 'line-through':''}} 
+  className="todo">{todo.text}
+  <div>
+   <button onClick={()=>completeTodo(index)}>Completed</button>
+   <button onClick={()=>removeTodo(index)}>x</button>
+   </div>
+   </div>;
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text className=input"
+        value={value}
+        placeholder="Add Todo"
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Introduction",
+      isCompleted: false
+    },
+    {
+      text: "Installation",
+      isCompleted: false
+    },
+    {
+      text: "Creating components",
+      isCompleted: false
+    },
+    {
+      text: "mutable states ",
+      isCompleted: false
+    },
+    {
+      text: "inmutable props ",
+      isCompleted: false
+    },
+    {
+      text: "Routing ",
+      isCompleted: false
+    },
+    {
+      text: "Styling ",
+      isCompleted: false
+    },
+    {
+      text: "Redux ",
+      isCompleted: false
+    },
+    {
+      text: "Firebase ",
+      isCompleted: false
+    },
+    {
+      text: "Hookes ",
+      isCompleted: false
+    },
+    {
+      text: "... ",
+      isCompleted: false
+    },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo=index=>{
+   const newTodos =[...todos];
+   newTodos[index].isCompleted=true;
+   setTodos(newTodos); 
+  }
+
+  const removeTodo=index=>{
+   const newTodos =[...todos];
+   newTodos.splice(index,1);
+   setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div><h1 className="text">LEARNING REACT</h1>
+    <div className="apps">
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo}/>
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
